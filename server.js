09d19f912f635e6416ee517e8522bcc832873806
Dinesh B S <dineshbs444@gmail.com>
2021-10-13 18:37:59 +0530

    Added event names and links to each of them

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const port = process.env.PORT || 4000;
 // Array of all the events
 var events = new Array();
 
-events.push(["Name 1", "Date & Time", "Place" , "0"]);
-events.push(["Name 1", "Date & Time", "Place" , "1"]);
-events.push(["Name 1", "Date & Time", "Place" , "2"]);
-events.push(["Name 1", "Date & Time", "Place" , "3"]);
-events.push(["Name 1", "Date & Time", "Place" , "4"]);
-events.push(["Name 1", "Date & Time", "Place" , "5"]);
-events.push(["Name 1", "Date & Time", "Place" , "6"]);
-events.push(["Name 1", "Date & Time", "Place" , "7"]);
-events.push(["Name 1", "Date & Time", "Place" , "8"]);
-events.push(["Name 1", "Date & Time", "Place" , "9"]);
+events.push(["Front Row Open Mics", "21st October, 2021", "Bengaluru" , "0", "https://in.bookmyshow.com/events/frontrow-open-mics/ET00313611"]);
+events.push(["The Laugh Store", "24th October, 2021", "Mumbai" , "1", "https://in.bookmyshow.com/events/the-laugh-store/ET00301197"]);
+events.push(["The Lineup", "31st October, 2021", "Chennai" , "2", "https://in.bookmyshow.com/events/the-lineup-ft-by-nishant-suri-onkar-yadav-rohit/ET00313888"]);
+events.push(["Please Interrupt", "2nd November, 2021", "Kochi" , "3", "https://in.bookmyshow.com/events/please-interrupt-by-nishant-tanwar/ET00315942"]);
+events.push(["Uncommon Sense", "4th November, 2021", "Bengaluru" , "4", "https://in.bookmyshow.com/events/uncommon-sense-with-jeeveshu-ahluwalia/ET00314791"]);
+events.push(["The Next Big Thing", "6th November, 2021", "Gurgaon" , "5", "https://in.bookmyshow.com/events/the-next-big-thing-standup-comedy-open-mic/ET00315769"]);
+events.push(["Tried n Tested", "8th November, 2021", "Chennai" , "6", "https://in.bookmyshow.com/events/tried-n-tested-a-standup-comedy-show/ET00315313"]);
+events.push(["Central Club of Comedy", "24th November, 2021", "Delhi" , "7", "https://in.bookmyshow.com/events/central-club-of-comedy/ET00307841"]);
+events.push(["Guide to the Galaxy", "5th December, 2021", "Bengaluru" , "8", "https://in.bookmyshow.com/events/guide-to-the-galaxy-stand-up-by-mohd-suhel/ET00315632"]);
+events.push(["Why so serious", "13th December, 2021", "Chennai" , "9", "https://in.bookmyshow.com/events/why-so-surious-by-nishant-suri/ET00308964"]);
 
 // All the booked users are stored in this HashMap
 var usersBooked = new Map();
@@ -43,7 +43,8 @@ app.get('/book_event', async (req, res) => {
         "name":"The Comedy Club",
         "time":"30th April, 2021",
         "place":"Arangum",
-        "eventId":"2"
+        "eventId":"2",
+        "link":"https://in.bookmyshow.com/events/central-club-of-comedy/ET00307841"
     };
     objects.push(obj);
     var sendData = {
@@ -71,7 +72,8 @@ app.get('/book_event', async (req, res) => {
             "name":arr[i][0],
             "time":arr[i][1],
             "place":arr[i][2],
-            "eventId":arr[i][3]
+            "eventId":arr[i][3],
+            "link":arr[i][4]
         };
         objects.push(obj);
     }
@@ -104,7 +106,8 @@ app.get('/book_event', async (req, res) => {
             "name":arr[i][0],
             "time":arr[i][1],
             "place":arr[i][2],
-            "eventId":arr[i][3]
+            "eventId":arr[i][3],
+            "link":arr[i][4]
         };
         objects.push(obj);
     }
